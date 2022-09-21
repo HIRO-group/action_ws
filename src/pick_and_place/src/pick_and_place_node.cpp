43a17d9710fdc53c284e1b4d662bef13580e2a2d
Nataliya <23035717+nataliya-dev@users.noreply.github.com>
2022-09-21 12:50:07 -0600

    trajectory generation, changing planner, and vis

--- src/pick_and_place/src/pick_and_place_node.cpp
+++ src/pick_and_place/src/pick_and_place_node.cpp
@@ -1,7 +1,6 @@
 
 #include <pluginlib/class_loader.h>
 #include <ros/ros.h>
-
 // MoveIt
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit/planning_interface/planning_interface.h>
@@ -13,12 +12,45 @@
 #include <moveit_msgs/PlanningScene.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
+constexpr char LOGNAME[] = "pick_and_place_node";
+
+void printPlannerConfigMap(
+    const planning_interface::PlannerConfigurationMap& planner_config_map) {
+  for (auto config : planner_config_map) {
+    ROS_INFO_NAMED(LOGNAME, "Map Name: %s", config.first.c_str());
+    ROS_INFO_NAMED(LOGNAME, "\tGroup: %s", config.second.group.c_str());
+    ROS_INFO_NAMED(LOGNAME, "\tName: %s", config.second.name.c_str());
+
+    for (auto setting : config.second.config) {
+      ROS_INFO_NAMED(LOGNAME, "\t\tSetting: %s", setting.first.c_str());
+      ROS_INFO_NAMED(LOGNAME, "\t\tValue: %s", setting.second.c_str());
+    }
+  }
+}
+
+void addPlannerConfigurationSettings(
+    planning_interface::PlannerConfigurationMap& planner_config_map,
+    const std::string& group_name,
+    const std::map<std::string, std::string>& setting_map) {
+  planning_interface::PlannerConfigurationSettings planner_settings;
+  planner_settings.group = group_name;
+  planner_settings.name = setting_map.at("type");
+  planner_settings.config = setting_map;
+  std::string key = group_name + "[" + setting_map.at("type") + "]";
+  planner_config_map[key] = planner_settings;
+}
+
 int main(int argc, char** argv) {
   ros::init(argc, argv, "pick_and_place_node");
   ros::AsyncSpinner spinner(1);
   spinner.start();
   ros::NodeHandle node_handle("~");
 
+  ROS_DEBUG_NAMED(LOGNAME, "Start!");
+
+  // Initialization
+  // ^^^^^^^^^^^^^^
+  // ^^^^^^^^^^^^^^
   robot_model_loader::RobotModelLoaderPtr robot_model_loader(
       new robot_model_loader::RobotModelLoader("robot_description"));
 
@@ -47,14 +79,15 @@ int main(int argc, char** argv) {
   moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(
       planning_scene_monitor::LockedPlanningSceneRO(psm)->getCurrentState()));
 
-  robot_state->printStateInfo();
+  // robot_state->printStateInfo();
 
   /* Create a JointModelGroup to keep track of the current robot pose and
      planning group. The Joint Model group is useful for dealing with one set of
      joints at a time such as a left arm or a end effector */
 
+  const std::string group_name = "panda_arm";
   const moveit::core::JointModelGroup* joint_model_group =
-      robot_state->getJointModelGroup("panda_link0");
+      robot_state->getJointModelGroup(group_name);
 
   // We can now setup the PlanningPipeline object, which will use the ROS
   // parameter server to determine the set of request adapters and the planning
@@ -63,7 +96,29 @@ int main(int argc, char** argv) {
       new planning_pipeline::PlanningPipeline(
           robot_model, node_handle, "planning_plugin", "request_adapters"));
 
+  const planning_interface::PlannerManagerPtr planner_manager =
+      planning_pipeline->getPlannerManager();
+
+  planning_interface::PlannerConfigurationMap planner_config_map;
+  std::map<std::string, std::string> setting_map;
+  setting_map["type"] = "geometric::EST";
+  setting_map["range"] = "0.0";
+  setting_map["goal_bias"] = "0.05";
+
+  addPlannerConfigurationSettings(planner_config_map, group_name, setting_map);
+
+  planner_manager->setPlannerConfigurations(planner_config_map);
+
+  printPlannerConfigMap(planner_config_map);
+
+  std::vector<std::string> planning_algos;
+  planner_manager->getPlanningAlgorithms(planning_algos);
+  for (auto algo : planning_algos) {
+    ROS_INFO_NAMED(LOGNAME, "Available planning algorithm %s", algo.c_str());
+  }
+
   // Pose Goal
+  // ^^^^^^^^^
   // ^^^^^^^^^
   // We will now create a motion plan request for the right arm of the Panda
   // specifying the desired pose of the end-effector as input.
@@ -71,15 +126,22 @@ int main(int argc, char** argv) {
   planning_interface::MotionPlanResponse res;
   geometry_msgs::PoseStamped pose;
   pose.header.frame_id = "panda_link0";
-  pose.pose.position.x = 0.3;
+  pose.pose.position.x = 0.5;
   pose.pose.position.y = 0.0;
   pose.pose.position.z = 0.75;
   pose.pose.orientation.w = 1.0;
+  pose.pose.orientation.x = 0.0;
+  pose.pose.orientation.y = 0.0;
+  pose.pose.orientation.z = 0.0;
 
   // A tolerance of 0.01 m is specified in position
   // and 0.01 radians in orientation
   std::vector<double> tolerance_pose(3, 0.01);
   std::vector<double> tolerance_angle(3, 0.01);
+
+  req.group_name = group_name;
+  req.allowed_planning_time = 5.0;
+  req.planner_id = "geometric::EST";
 
   moveit_msgs::Constraints pose_goal =
       kinematic_constraints::constructGoalConstraints(
@@ -99,6 +161,86 @@ int main(int argc, char** argv) {
     ROS_ERROR("Could not compute plan successfully");
     return 0;
   }
+
+  // Visualize the result
+  // ^^^^^^^^^^^^^^^^^^^^
+  // ^^^^^^^^^^^^^^^^^^^^
+  // The package MoveItVisualTools provides many capabilities for visualizing
+  // objects, robots, and trajectories in RViz as well as debugging tools such
+  // as step-by-step introspection of a script.
+  namespace rvt = rviz_visual_tools;
+  moveit_visual_tools::MoveItVisualTools visual_tools("panda_link0");
+  visual_tools.deleteAllMarkers();
+
+  ros::Publisher display_publisher =
+      node_handle.advertise<moveit_msgs::DisplayTrajectory>(
+          "/move_group/display_planned_path", 1, true);
+  moveit_msgs::DisplayTrajectory display_trajectory;
+
+  /* Visualize the trajectory */
+  ROS_INFO("Visualizing the trajectory");
+  moveit_msgs::MotionPlanResponse response;
+  res.getMessage(response);
+
+  display_trajectory.trajectory_start = response.trajectory_start;
+  display_trajectory.trajectory.push_back(response.trajectory);
+  display_publisher.publish(display_trajectory);
+  visual_tools.publishTrajectoryLine(display_trajectory.trajectory.back(),
+                                     joint_model_group);
+  visual_tools.trigger();
+
+  // Move Robot
+  // ^^^^^^^^^^^^^^^^^^^^
+  // ^^^^^^^^^^^^^^^^^^^^
+  moveit_msgs::RobotTrajectory robot_trajectory = response.trajectory;
+  trajectory_msgs::JointTrajectory joint_trajectory =
+      robot_trajectory.joint_trajectory;
+
+  ROS_INFO_NAMED(LOGNAME, "Num joints: %ld",
+                 joint_trajectory.joint_names.size());
+  ROS_INFO_NAMED(LOGNAME, "Num points: %ld", joint_trajectory.points.size());
+
+  for (int i = 0; i < joint_trajectory.joint_names.size(); i++) {
+    std::string name = joint_trajectory.joint_names[i];
+    std::cout << name << " ";
+  }
+
+  for (int i = 0; i < joint_trajectory.points.size(); i++) {
+    trajectory_msgs::JointTrajectoryPoint traj_point =
+        joint_trajectory.points[i];
+    std::cout << "\npositions" << std::endl;
+    for (int k = 0; k < traj_point.positions.size(); k++) {
+      double position = traj_point.positions[k];
+      std::cout << position << " ";
+    }
+
+    std::cout << "\nvelocities" << std::endl;
+    for (int k = 0; k < traj_point.velocities.size(); k++) {
+      double velocity = traj_point.velocities[k];
+      std::cout << velocity << " ";
+    }
+
+    std::cout << "\naccelerations" << std::endl;
+    for (int k = 0; k < traj_point.accelerations.size(); k++) {
+      double accel = traj_point.accelerations[k];
+      std::cout << accel << " ";
+    }
+
+    std::cout << "\neffort" << std::endl;
+    for (int k = 0; k < traj_point.effort.size(); k++) {
+      double effort = traj_point.effort[k];
+      std::cout << effort << " ";
+    }
+  }
+
+  std::cout << std::endl;
+
+  // std_msgs/Header header
+  // string[] joint_names
+  // trajectory_msgs/JointTrajectoryPoint[] points
+
+  std::cout << "Press any key to continue: ";
+  getchar();
 
   std::cout << "Finished!" << std::endl;
   return 0;
