0077eb3cc6d8bccda5e02613b1770584a496ebeb
Nataliya <23035717+nataliya-dev@users.noreply.github.com>
2022-09-30 17:10:17 -0600

    ability to change planner in simple setup

--- src/pick_and_place/src/pick_and_place_node.cpp
+++ src/pick_and_place/src/pick_and_place_node.cpp
@@ -4,22 +4,21 @@
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit/ompl_interface/detail/state_validity_checker.h>
 #include <moveit/ompl_interface/model_based_planning_context.h>
+#include <moveit/ompl_interface/ompl_interface.h>
 #include <moveit/ompl_interface/parameterization/joint_space/joint_model_state_space.h>
-#include <moveit/ompl_interface/parameterization/joint_space/joint_model_state_space_factory.h>
-#include <moveit/ompl_interface/parameterization/model_based_state_space.h>
-#include <moveit/ompl_interface/parameterization/work_space/pose_model_state_space_factory.h>
 #include <moveit/planning_interface/planning_interface.h>
 #include <moveit/planning_pipeline/planning_pipeline.h>
 #include <moveit/planning_scene/planning_scene.h>
 #include <moveit/planning_scene_monitor/current_state_monitor.h>
 #include <moveit/planning_scene_monitor/planning_scene_monitor.h>
-#include <moveit/trajectory_execution_manager/trajectory_execution_manager.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 #include <moveit_msgs/PlanningScene.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
-#include <ompl/base/Goal.h>
 #include <ompl/geometric/SimpleSetup.h>
+#include <ompl/geometric/planners/rrt/RRTConnect.h>
 #include <ompl/geometric/planners/rrt/VFRRT.h>
+
+#include "ompl/geometric/planners/est/EST.h"
 
 constexpr char LOGNAME[] = "pick_and_place_node";
 
@@ -77,25 +76,6 @@ void printJointTrajectory(
   }
 }
 
-void printControllers(
-    const moveit_controller_manager::MoveItControllerManagerPtr&
-        controller_manager) {
-  std::vector<std::string> active_controllers;
-  controller_manager->getActiveControllers(active_controllers);
-  std::vector<std::string> known_controllers;
-  controller_manager->getControllersList(known_controllers);
-
-  ROS_INFO_NAMED(LOGNAME, "Active controllers: %ld", active_controllers.size());
-  for (auto controller : active_controllers) {
-    std::cout << controller << ", " << std::endl;
-  }
-
-  ROS_INFO_NAMED(LOGNAME, "Known controllers: %ld", known_controllers.size());
-  for (auto controller : known_controllers) {
-    std::cout << controller << ", " << std::endl;
-  }
-}
-
 void promptAnyInput() {
   std::cout << std::endl;
   std::cout << "Press any key to continue: ";
@@ -135,75 +115,142 @@ void setCurToStartState(
   req.start_state.joint_state.effort = start_joint_values;
 }
 
-void plannerSetup(const moveit::core::RobotModelPtr& robot_model,
-                  const std::string& group_name) {
+std::unique_ptr<ompl_interface::OMPLInterface> getOMPLInterface(
+    const moveit::core::RobotModelConstPtr& model, const ros::NodeHandle& nh) {
+  std::unique_ptr<ompl_interface::OMPLInterface> ompl_interface =
+      std::make_unique<ompl_interface::OMPLInterface>(model, nh);
+  return ompl_interface;
+}
+
+ompl_interface::ModelBasedPlanningContextPtr createPlanningContext(
+    const planning_scene::PlanningSceneConstPtr& planning_scene,
+    const moveit::core::RobotModelPtr& robot_model,
+    const moveit_msgs::MotionPlanRequest& req, const ros::NodeHandle& nh) {
+  const std::string group_name = "panda_arm";
   ompl_interface::ModelBasedStateSpaceSpecification space_spec(robot_model,
                                                                group_name);
   ompl_interface::ModelBasedStateSpacePtr state_space =
       std::make_shared<ompl_interface::JointModelStateSpace>(space_spec);
   state_space->computeLocations();
-  // ompl_interface::ModelBasedStateSpaceFactoryPtr factory(
-  //     ompl_interface::ModelBasedStateSpaceFactoryPtr(
-  //         new ompl_interface::JointModelStateSpaceFactory()));
-  // ompl_interface::ModelBasedStateSpaceFactoryPtr factory(
-  //     ModelBasedStateSpaceFactoryPtr(new PoseModelStateSpaceFactory()));
+
+  std::unique_ptr<ompl_interface::OMPLInterface> ompl_interface =
+      getOMPLInterface(robot_model, nh);
+
+  planning_interface::PlannerConfigurationMap pconfig_map =
+      ompl_interface->getPlannerConfigurations();
+  // printPlannerConfigMap(pconfig_map);
+
+  ompl_interface::PlanningContextManager planning_context_manager =
+      ompl_interface->getPlanningContextManager();
+
+  auto pc = pconfig_map.end();
+  pc = pconfig_map.find(req.planner_id);
+  if (pc == pconfig_map.end()) {
+    ROS_WARN_NAMED(LOGNAME,
+                   "Cannot find planning configuration for group '%s' using "
+                   "planner '%s'. Will use defaults instead.",
+                   req.group_name.c_str(), req.planner_id.c_str());
+    return ompl_interface::ModelBasedPlanningContextPtr();
+  } else {
+    ROS_INFO_NAMED(
+        LOGNAME, "Found planning configuration for group %s, using planner %s.",
+        req.group_name.c_str(), req.planner_id.c_str());
+  }
+
+  planning_interface::PlannerConfigurationSettings pconfig_settings =
+      pc->second;
+
+  ompl_interface::ModelBasedPlanningContextSpecification context_spec;
+  context_spec.config_ = pconfig_settings.config;
+  context_spec.planner_selector_ =
+      planning_context_manager.getPlannerSelector();
+  context_spec.constraint_sampler_manager_ =
+      std::make_shared<constraint_samplers::ConstraintSamplerManager>();
+  context_spec.state_space_ = state_space;
+
   ompl::geometric::SimpleSetupPtr ompl_simple_setup =
       std::make_shared<ompl::geometric::SimpleSetup>(state_space);
+  context_spec.ompl_simple_setup_ = ompl_simple_setup;
 
-  ompl::base::SpaceInformationPtr si = ompl_simple_setup->getSpaceInformation();
-  ompl::base::PlannerPtr planner(new ompl::geometric::RRT(si));
-  ompl_simple_setup->setPlanner(planner);
+  ompl_interface::ModelBasedPlanningContextPtr context =
+      std::make_shared<ompl_interface::ModelBasedPlanningContext>(group_name,
+                                                                  context_spec);
 
-  ompl::base::ScopedState<> ompl_start_state(state_space);
-  moveit::core::RobotState complete_initial_robot_state =
-      state_space->getRobotModel();
-  state_space->copyToOMPLState(ompl_start_state.get(),
-                               complete_initial_robot_state);
-  ompl_simple_setup->setStartState(ompl_start_state);
+  unsigned int max_goal_samples_ = 10;
+  unsigned int max_state_sampling_attempts_ = 4;
+  unsigned int max_goal_sampling_attempts_ = 1000;
+  unsigned int max_planning_threads_ = 4;
+  double max_solution_segment_length_ = 0.0;
+  unsigned int minimum_waypoint_count_ = 2;
 
-  ompl_interface::ModelBasedPlanningContextSpecification planning_context_spec;
-  planning_context_spec.state_space_ = state_space;
-  planning_context_spec.ompl_simple_setup_ = ompl_simple_setup;
-  ompl_interface::ModelBasedPlanningContextPtr planning_context =
-      std::make_shared<ompl_interface::ModelBasedPlanningContext>(
-          group_name, planning_context_spec);
-  planning_scene::PlanningScenePtr planning_scene =
-      std::make_shared<planning_scene::PlanningScene>(robot_model);
-  planning_context->setPlanningScene(planning_scene);
-  moveit::core::RobotState start_state(robot_model);
-  start_state.setToDefaultValues();
-  planning_context->setCompleteInitialState(start_state);
-  ompl::base::StateValidityCheckerPtr svc(ompl::base::StateValidityCheckerPtr(
-      new ompl_interface::StateValidityChecker(planning_context.get())));
-  svc->isValid(ompl_start_state.get());
-  ompl_simple_setup->setStateValidityChecker(svc);
+  context->setMaximumPlanningThreads(max_planning_threads_);
+  context->setMaximumGoalSamples(max_goal_samples_);
+  context->setMaximumStateSamplingAttempts(max_state_sampling_attempts_);
+  context->setMaximumGoalSamplingAttempts(max_goal_sampling_attempts_);
+  if (max_solution_segment_length_ >
+      std::numeric_limits<double>::epsilon()) {  // wtf is this
+    context->setMaximumSolutionSegmentLength(max_solution_segment_length_);
+  }
+  context->setMinimumWaypointCount(minimum_waypoint_count_);
 
-  planning_interface::MotionPlanRequest req;
-  geometry_msgs::PoseStamped pose;
-  pose.header.frame_id = "panda_link0";
-  pose.pose.position.x = 0.5;
-  pose.pose.position.y = 0.0;
-  pose.pose.position.z = 0.75;
-  pose.pose.orientation.w = 1.0;
-  pose.pose.orientation.x = 0.0;
-  pose.pose.orientation.y = 0.0;
-  pose.pose.orientation.z = 0.0;
+  context->clear();
 
-  moveit_msgs::MoveItErrorCodes* error_code;
+  moveit::core::RobotStatePtr start_state =
+      planning_scene->getCurrentStateUpdated(req.start_state);
 
-  planning_context->setGoalConstraints(req.goal_constraints,
-                                       req.path_constraints, error_code);
+  context->setPlanningScene(planning_scene);
+  context->setMotionPlanRequest(req);
+  context->setCompleteInitialState(*start_state);
 
-  ompl::geometric::SimpleSetupPtr context_ss =
-      planning_context->getOMPLSimpleSetup();
+  context->setPlanningVolume(req.workspace_parameters);
+  moveit_msgs::MoveItErrorCodes error_code;
+  if (!context->setPathConstraints(req.path_constraints, &error_code)) {
+    ROS_ERROR_NAMED(LOGNAME, "context->setPathConstraints() error");
+    return ompl_interface::ModelBasedPlanningContextPtr();
+  }
 
-  ompl::base::GoalPtr goal = context_ss->getGoal();
+  if (!context->setGoalConstraints(req.goal_constraints, req.path_constraints,
+                                   &error_code)) {
+    ROS_ERROR_NAMED(LOGNAME, "context->setGoalConstraints() error");
+    return ompl_interface::ModelBasedPlanningContextPtr();
+  }
 
-  ompl_simple_setup->setGoal(goal);
+  bool use_constraints_approximation = true;
+  try {
+    context->configure(nh, use_constraints_approximation);
+    ROS_INFO_NAMED(LOGNAME, "%s: New planning context is set.",
+                   context->getName().c_str());
+    error_code.val = moveit_msgs::MoveItErrorCodes::SUCCESS;
+  } catch (ompl::Exception& ex) {
+    ROS_ERROR_NAMED(LOGNAME, "OMPL encountered an error: %s", ex.what());
+    context.reset();
+  }
 
-  // see if it will be possible to set the simple set up into the context after
-  // having it configured compare these results with the results obtained by
-  // just going through the ompl planning library
+  return context;
+}
+
+ompl::base::PlannerPtr createPlanner(
+    const ompl::base::SpaceInformationPtr& si) {
+  ompl::base::PlannerPtr planner =
+      std::make_shared<ompl::geometric::RRTConnect>(si);
+  return planner;
+}
+
+void changePlanner(ompl_interface::ModelBasedPlanningContextPtr& context) {
+  ompl::geometric::SimpleSetupPtr simple_setup = context->getOMPLSimpleSetup();
+  ompl::base::SpaceInformationPtr si = simple_setup->getSpaceInformation();
+  ompl::base::PlannerPtr planner = createPlanner(si);
+  simple_setup->setPlanner(planner);
+}
+
+bool generatePlan(const ompl_interface::ModelBasedPlanningContextPtr& context,
+                  planning_interface::MotionPlanResponse& res) {
+  bool is_solved = context->solve(res);
+  std::size_t state_count = res.trajectory_->getWayPointCount();
+  ROS_DEBUG_STREAM("Motion planner reported a solution path with "
+                   << state_count << " states");
+  ROS_INFO_NAMED(LOGNAME, "Is plan generated? %d", is_solved);
+  return is_solved;
 }
 
 int main(int argc, char** argv) {
@@ -237,23 +284,23 @@ int main(int argc, char** argv) {
    * the robot's kinematic information */
   moveit::core::RobotModelPtr robot_model = robot_model_loader->getModel();
 
-  /* We can get the most up to date robot state from the PlanningSceneMonitor by
-     locking the internal planning scene for reading. This lock ensures that the
-     underlying scene isn't updated while we are reading it's state.
-     RobotState's are useful for computing the forward and inverse kinematics of
-     the robot among many other uses */
-  // moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(
-  //     planning_scene_monitor::LockedPlanningSceneRO(psm)->getCurrentState()));
+  const std::string group_name = "panda_arm";
+
+  /* We can get the most up to date robot state from the PlanningSceneMonitor
+     by locking the internal planning scene for reading. This lock ensures
+     that the underlying scene isn't updated while we are reading it's state.
+     RobotState's are useful for computing the forward and inverse kinematics
+     of the robot among many other uses */
 
   planning_scene_monitor::CurrentStateMonitorPtr csm = psm->getStateMonitor();
   csm->enableCopyDynamics(true);
 
-  moveit::core::RobotStatePtr robot_state = csm->getCurrentState();
+  moveit::core::RobotStatePtr robot_state(new moveit::core::RobotState(
+      planning_scene_monitor::LockedPlanningSceneRO(psm)->getCurrentState()));
 
   /* Create a JointModelGroup to keep track of the current robot pose and
-     planning group. The Joint Model group is useful for dealing with one set of
-     joints at a time such as a left arm or a end effector */
-  const std::string group_name = "panda_arm";
+     planning group. The Joint Model group is useful for dealing with one set
+     of joints at a time such as a left arm or a end effector */
   const moveit::core::JointModelGroup* joint_model_group =
       robot_state->getJointModelGroup(group_name);
 
@@ -269,15 +316,17 @@ int main(int argc, char** argv) {
   ROS_INFO_NAMED(LOGNAME, "Robot State Positions");
   robot_state->printStatePositions();
 
-  // We can now setup the PlanningPipeline object, which will use the ROS
-  // parameter server to determine the set of request adapters and the planning
-  // plugin to use
-  planning_pipeline::PlanningPipelinePtr planning_pipeline(
-      new planning_pipeline::PlanningPipeline(
-          robot_model, node_handle, "planning_plugin", "request_adapters"));
+  // // We can now setup the PlanningPipeline object, which will use the ROS
+  // // parameter server to determine the set of request adapters and the
+  // planning
+  // // plugin to use
+  // planning_pipeline::PlanningPipelinePtr planning_pipeline(
+  //     new planning_pipeline::PlanningPipeline(
+  //         robot_model, node_handle, "planning_plugin",
+  //         "request_adapters"));
 
-  const planning_interface::PlannerManagerPtr planner_manager =
-      planning_pipeline->getPlannerManager();
+  // const planning_interface::PlannerManagerPtr planner_manager =
+  //     planning_pipeline->getPlannerManager();
 
   // Pose Goal
   // ^^^^^^^^^
@@ -305,7 +354,7 @@ int main(int argc, char** argv) {
 
   req.group_name = group_name;
   req.allowed_planning_time = 5.0;
-  req.planner_id = "panda_arm[EST]";
+  req.planner_id = "panda_arm[RRT]";
 
   moveit_msgs::Constraints pose_goal =
       kinematic_constraints::constructGoalConstraints(
@@ -314,17 +363,24 @@ int main(int argc, char** argv) {
 
   // Before planning, we will need a Read Only lock on the planning scene so
   // that it does not modify the world representation while planning
-  {
-    planning_scene_monitor::LockedPlanningSceneRO lscene(psm);
-    /* Now, call the pipeline and check whether planning was successful. */
-    planning_pipeline->generatePlan(lscene, req, res);
-  }
-  /* Now, call the pipeline and check whether planning was successful. */
-  /* Check that the planning was successful */
-  if (res.error_code_.val != res.error_code_.SUCCESS) {
-    ROS_ERROR("Could not compute plan successfully");
-    return 0;
-  }
+  planning_scene_monitor::LockedPlanningSceneRO lscene(psm);
+
+  ompl_interface::ModelBasedPlanningContextPtr context =
+      createPlanningContext(lscene, robot_model, req, node_handle);
+
+  changePlanner(context);
+
+  generatePlan(context, res);
+
+  // /* Now, call the pipeline and check whether planning was successful. */
+  // planning_pipeline->generatePlan(lscene, req, res);
+
+  // /* Now, call the pipeline and check whether planning was successful. */
+  // /* Check that the planning was successful */
+  // if (res.error_code_.val != res.error_code_.SUCCESS) {
+  //   ROS_ERROR("Could not compute plan successfully");
+  //   return 0;
+  // }
 
   // Visualize the result
   // ^^^^^^^^^^^^^^^^^^^^
@@ -357,30 +413,6 @@ int main(int argc, char** argv) {
   // Execute Trajectory
   // ^^^^^^^^^^^^^^^^^^^^
   // ^^^^^^^^^^^^^^^^^^^^
-  moveit_msgs::RobotTrajectory robot_trajectory = response.trajectory;
-  trajectory_msgs::JointTrajectory joint_trajectory =
-      robot_trajectory.joint_trajectory;
-
-  trajectory_execution_manager::TrajectoryExecutionManagerPtr
-      traj_execution_manager(
-          new trajectory_execution_manager::TrajectoryExecutionManager(
-              robot_model, csm));
-
-  moveit_controller_manager::MoveItControllerManagerPtr controller_manager =
-      traj_execution_manager->getControllerManager();
-
-  printControllers(controller_manager);
-
-  traj_execution_manager->clear();
-
-  traj_execution_manager->push(robot_trajectory);
-  moveit_controller_manager::ExecutionStatus status =
-      traj_execution_manager->executeAndWait();
-
-  ROS_INFO_NAMED(LOGNAME, "Status: %s", status.asString().c_str());
-
-  ROS_INFO_NAMED(LOGNAME, "Robot State Positions");
-  robot_state->printStatePositions();
 
   promptAnyInput();
 
---
+++ upstream_sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eo pipefail
+
+SUDO="sudo -H"
+
+fetch_and_merge(){
+BRANCH="$(git config -f $toplevel/.gitmodules submodule.$name.branch)";
+
+if git ls-remote --exit-code upstream; then
+  git fetch upstream
+  git merge upstream/$(BRANCH)
+else
+  git remote add upstream git@github.com:ompl/omplapp.git
+fi
+
+}
